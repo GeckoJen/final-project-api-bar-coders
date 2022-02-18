8400ec2570827f5d3804853ffe5aa75f68043abe
codingmohamed <cloudchronicles92@gmail.com>
2022-02-18 11:19:45 +0000

    branches set up

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 import express from "express";
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "I wish we had some information to give you ☹️" });
+// /* GET users listing. */
+// router.get("/", function (req, res, next) {
+//   res.json({ message: "I wish we had some information to give you ☹️" });
+// });
+
+// GET student details for homepage
+router.get("/", async function (req, res) {
+      const studentData = await getStudentData();
+      res.json({
+            success: true,
+            payload: studentData,
+      });
 });
 
 export default router;
