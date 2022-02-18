ddaa5f7290197c95473735bae96b7f2e3e42e178
Alex Platt <alja.platt@gmail.com>
2022-02-18 14:52:46 +0000

    models plan

---
+++ models/index.js
@@ -0,0 +1,24 @@
+// models tbc
+
+getStudentData;
+updateBook;
+newBook;
+noBook;
+completeBooks;
+dictionary;
+newWord;
+messages;
+getAllStudentsAndTeacher;
+getWholeClassSevenStat;
+getWholeClassPages;
+getWholeClassMinutes;
+getWholeClassBooksFinished;
+individualSevenStat;
+individualPages;
+individualMinutes;
+individualBooksFinished;
+classMessageLog;
+newStudentFeedback;
+studentMessageLog;
+studentSummaries;
+newClassFeedback;
