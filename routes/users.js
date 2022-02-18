61336e3186beeafbd08148da059aee52ac84283f
codingmohamed <cloudchronicles92@gmail.com>
2022-02-18 11:45:03 +0000

    partial routes

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,44 @@ const router = express.Router();
 //   res.json({ message: "I wish we had some information to give you ☹️" });
 // });
 
-// GET student details for homepage
-router.get("/", async function (req, res) {
+// GET student details for homepage and log your reading page
+router.get("/homepage", async function (req, res) {
       const studentData = await getStudentData();
       res.json({
             success: true,
             payload: studentData,
+      });
+});
+
+// POST for log your reading page
+router.post("/readingpage", async function (req, res) {
+      const { current_page, minutes_read, summary, isComplete } = req.body;
+      const updateBook = await updateBook(
+            current_page,
+            minutes_read,
+            summary,
+            isComplete
+      );
+      res.json({
+            success: true,
+            payload: updateBook,
+      });
+});
+
+// POST for new book pageB
+
+router.post("/newbook", async function (req, res) {
+      const { student_id, title, cover, author, total_pages } = req.body;
+      const newBook = await newBook(
+            student_id,
+            title,
+            cover,
+            author,
+            total_pages
+      );
+      res.json({
+            success: true,
+            payload: newBook,
       });
 });
 
