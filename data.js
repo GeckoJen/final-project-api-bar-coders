15534ea69e5c5a7d15f649b49af5f63a40b63eca
AsmaDage <asmadage@gmail.com>
2022-02-18 14:00:51 +0000

    data file and structure created

---
+++ data.js
@@ -0,0 +1,13 @@
+const teacherData = [{}];
+
+const studentData = [{}];
+
+const classFeedbackData = [{}];
+
+const feedbackData = [{}];
+
+const dictionaryData = [{}];
+
+const allBooksData = [{}];
+
+const summaryData = [{}];
