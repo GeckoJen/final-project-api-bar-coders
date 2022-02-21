1bccc970a2bd107eaf14302f333672717e435990
codingmohamed <cloudchronicles92@gmail.com>
2022-02-21 09:35:28 +0000

    functions exported

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,29 @@
 import express from "express";
 const router = express.Router();
+import {
+      getStudentData,
+      updateBook,
+      newBook,
+      noBook,
+      completeBooks,
+      dictionary,
+      newWord,
+      messages,
+      getAllStudentsAndTeacher,
+      getWholeClassSevenStat,
+      getWholeClassPages,
+      getWholeClassMinutes,
+      getWholeClassBooksFinished,
+      individualSevenStat,
+      individualPages,
+      individualMinutes,
+      individualBooksFinished,
+      classMessageLog,
+      newStudentFeedback,
+      studentMessageLog,
+      studentSummaries,
+      newClassFeedback,
+} from "../models/index.js";
 
 // /* GET users listing. */
 // router.get("/", function (req, res, next) {
@@ -8,163 +32,169 @@ const router = express.Router();
 
 // GET student details for homepage and log your reading page
 router.get("/studenthome", async function (req, res) {
-  const studentData = await getStudentData();
-  res.json({
-    success: true,
-    payload: studentData,
-  });
+      const studentData = await getStudentData();
+      res.json({
+            success: true,
+            payload: studentData,
+      });
 });
 
 // POST for log your reading page
 router.post("/readingpage", async function (req, res) {
-  const { current_page, minutes_read, summary, isComplete } = req.body;
-  const updateBook = await updateBook(
-    current_page,
-    minutes_read,
-    summary,
-    isComplete
-  );
-  res.json({
-    success: true,
-    payload: updateBook,
-  });
+      const { current_page, minutes_read, summary, isComplete } = req.body;
+      const updateBook = await updateBook(
+            current_page,
+            minutes_read,
+            summary,
+            isComplete
+      );
+      res.json({
+            success: true,
+            payload: updateBook,
+      });
 });
 
 // POST for new book pageB
 
 router.post("/newbook", async function (req, res) {
-  const { student_id, title, cover, author, total_pages } = req.body;
-  const newBook = await newBook(student_id, title, cover, author, total_pages);
-  res.json({
-    success: true,
-    payload: newBook,
-  });
+      const { student_id, title, cover, author, total_pages } = req.body;
+      const newBook = await newBook(
+            student_id,
+            title,
+            cover,
+            author,
+            total_pages
+      );
+      res.json({
+            success: true,
+            payload: newBook,
+      });
 });
 
 // Cant find new book
 
 router.post("/nobook", async function (req, res) {
-  const { student_id, title, author, total_pages } = req.body;
-  const noBook = await noBook(student_id, title, author, total_pages);
-  res.json({
-    success: true,
-    payload: noBook,
-  });
+      const { student_id, title, author, total_pages } = req.body;
+      const noBook = await noBook(student_id, title, author, total_pages);
+      res.json({
+            success: true,
+            payload: noBook,
+      });
 });
 
 // completed books
 
 router.get("/completebooks", async function (req, res) {
-  const completeBooks = await completeBooks();
-  res.json({
-    success: true,
-    payload: completeBooks,
-  });
+      const completeBooks = await completeBooks();
+      res.json({
+            success: true,
+            payload: completeBooks,
+      });
 });
 
 // dictionary
 router.get("/dictionary", async function (req, res) {
-  const dictionary = await dictionary();
-  res.json({
-    success: true,
-    payload: dictionary,
-  });
+      const dictionary = await dictionary();
+      res.json({
+            success: true,
+            payload: dictionary,
+      });
 });
 
 // add to dictionary
 
 router.post("/dictionary", async function (req, res) {
-  const { word } = req.body;
-  const newWord = await newWord(word);
-  res.json({
-    success: true,
-    payload: newWord,
-  });
+      const { word } = req.body;
+      const newWord = await newWord(word);
+      res.json({
+            success: true,
+            payload: newWord,
+      });
 });
 
 // new word or old word - fetch to api - if input value api request
 
 router.get("/messages", async function (req, res) {
-  const messages = await messages();
-  res.json({
-    success: true,
-    payload: messages,
-  });
+      const messages = await messages();
+      res.json({
+            success: true,
+            payload: messages,
+      });
 });
 
 // teacher dashboard - whole class info
 
 router.get("/teacherhome", async function (req, res) {
-  const allStudentsAndTeacher = await getAllStudentsAndTeacher();
-  const wholeClassSevenStat = await getWholeClassSevenStat();
-  const wholeClassPages = await getWholeClassPages();
-  const wholeClassMinutes = await getWholeClassMinutes();
-  const wholeClassBooksFinished = await getWholeClassBooksFinished();
-  res.json({
-    success: true,
-    payload: [
-      allStudentsAndTeacher,
-      wholeClassSevenStat,
-      wholeClassPages,
-      wholeClassMinutes,
-      wholeClassBooksFinished,
-    ],
-  });
+      const allStudentsAndTeacher = await getAllStudentsAndTeacher();
+      const wholeClassSevenStat = await getWholeClassSevenStat();
+      const wholeClassPages = await getWholeClassPages();
+      const wholeClassMinutes = await getWholeClassMinutes();
+      const wholeClassBooksFinished = await getWholeClassBooksFinished();
+      res.json({
+            success: true,
+            payload: [
+                  allStudentsAndTeacher,
+                  wholeClassSevenStat,
+                  wholeClassPages,
+                  wholeClassMinutes,
+                  wholeClassBooksFinished,
+            ],
+      });
 });
 
 // teacher dashboard - individual info
 
 router.get("/teacherstudentview", async function (req, res) {
-  const individualSevenStat = await getindividualSevenStat();
-  const individualPages = await getindividualPages();
-  const individualMinutes = await getindividualMinutes();
-  const individualBooksFinished = await getindividualBooksFinished();
-  res.json({
-    success: true,
-    payload: [
-      individualSevenStat,
-      individualPages,
-      individualMinutes,
-      individualBooksFinished,
-    ],
-  });
+      const individualSevenStat = await getindividualSevenStat();
+      const individualPages = await getindividualPages();
+      const individualMinutes = await getindividualMinutes();
+      const individualBooksFinished = await getindividualBooksFinished();
+      res.json({
+            success: true,
+            payload: [
+                  individualSevenStat,
+                  individualPages,
+                  individualMinutes,
+                  individualBooksFinished,
+            ],
+      });
 });
 
 // teacher dashboard - whole class MORE info
 
 router.get("/wholeclassmoreinfo", async function (req, res) {
-  const classMessageLog = await getClassMessageLog();
-  res.json({
-    success: true,
-    payload: classMessageLog,
-  });
+      const classMessageLog = await getClassMessageLog();
+      res.json({
+            success: true,
+            payload: classMessageLog,
+      });
 });
 
 router.post("/wholeclassmoreinfo", async function (req, res) {
-  const { feedback_text } = req.body;
-  const newClassFeedback = await newClassFeedback(feedback_text);
-  res.json({
-    success: true,
-    payload: newClassFeedback,
-  });
+      const { feedback_text } = req.body;
+      const newClassFeedback = await newClassFeedback(feedback_text);
+      res.json({
+            success: true,
+            payload: newClassFeedback,
+      });
 });
 
 router.get("/studentmoreinfo", async function (req, res) {
-  const studentMessageLog = await getStudentMessageLog();
-  const studentSummaries = await getStudentSummaries();
-  res.json({
-    success: true,
-    payload: [studentMessageLog, studentSummaries],
-  });
+      const studentMessageLog = await getStudentMessageLog();
+      const studentSummaries = await getStudentSummaries();
+      res.json({
+            success: true,
+            payload: [studentMessageLog, studentSummaries],
+      });
 });
 
 router.post("/studentmoreinfo", async function (req, res) {
-  const { feedback_text } = req.body;
-  const newStudentFeedback = await newStudentFeedback(feedback_text);
-  res.json({
-    success: true,
-    payload: newStudentFeedback,
-  });
+      const { feedback_text } = req.body;
+      const newStudentFeedback = await newStudentFeedback(feedback_text);
+      res.json({
+            success: true,
+            payload: newStudentFeedback,
+      });
 });
 
 export default router;
