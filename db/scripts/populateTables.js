3f8e99e498be8d782cf1ff05d45321990eaf31bf
AsmaDage <asmadage@gmail.com>
2022-02-21 10:12:00 +0000

    scripts tables added

--- db/scripts/populateTables.js
+++ db/scripts/populateTables.js
@@ -1,12 +1,4 @@
-import {
-  teacherData,
-  studentData,
-  dictionaryData,
-  feedbackData,
-  classFeedbackData,
-  allBooksData,
-  summaryData,
-} from "../../data.js";
+import { teacherData } from "../../data.js";
 import query from "../index.js";
 
 async function populateTeachersTable() {
@@ -22,87 +14,87 @@ async function populateTeachersTable() {
 }
 populateTeachersTable();
 
-async function populateStudentsTable() {
-  const sqlString = `INSERT INTO students (id,name,avatar,class,teacher_id) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
-  studentData.map(async function (data) {
-    let populatedData = await query(sqlString, [
-      data.id,
-      data.name,
-      data.avatar,
-      data.class,
-      data.teacher_id,
-    ]);
-    console.log(populatedData);
-  });
-}
-populateStudentsTable();
+// async function populateStudentsTable() {
+//   const sqlString = `INSERT INTO students (id,name,avatar,class,teacher_id) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+//   studentData.map(async function (data) {
+//     let populatedData = await query(sqlString, [
+//       data.id,
+//       data.name,
+//       data.avatar,
+//       data.class,
+//       data.teacher_id,
+//     ]);
+//     console.log(populatedData);
+//   });
+// }
+// populateStudentsTable();
 
-async function populateClassFeedback() {
-  const sqlString = `INSERT INTO classfeedback (class, feedback_text,teacher_id) VALUES ($1,$2,$3) RETURNING *;`;
-  classFeedbackData.map(async function (data) {
-    let populatedData = await query(sqlString, [
-      data.class,
-      data.feedback_text,
-      data.teacher_id,
-    ]);
-    console.log(populatedData);
-  });
-}
-populateClassFeedback();
+// async function populateClassFeedback() {
+//   const sqlString = `INSERT INTO classfeedback (class, feedback_text,teacher_id) VALUES ($1,$2,$3) RETURNING *;`;
+//   classFeedbackData.map(async function (data) {
+//     let populatedData = await query(sqlString, [
+//       data.class,
+//       data.feedback_text,
+//       data.teacher_id,
+//     ]);
+//     console.log(populatedData);
+//   });
+// }
+// populateClassFeedback();
 
-async function populateFeedbackTable() {
-  const sqlString = `INSERT INTO feedback (student_id, feedback_text) VALUES ($1,$2) RETURNING *;`;
-  feedbackData.map(async function (data) {
-    let populatedData = await query(sqlString, [
-      data.student_id,
-      data.feedback_text,
-    ]);
-    console.log(populatedData);
-  });
-}
-populateFeedbackTable();
+// async function populateFeedbackTable() {
+//   const sqlString = `INSERT INTO feedback (student_id, feedback_text) VALUES ($1,$2) RETURNING *;`;
+//   feedbackData.map(async function (data) {
+//     let populatedData = await query(sqlString, [
+//       data.student_id,
+//       data.feedback_text,
+//     ]);
+//     console.log(populatedData);
+//   });
+// }
+// populateFeedbackTable();
 
-async function populateDictionaryData() {
-  const sqlString = `INSERT INTO dictionary (student_id, word,definition) VALUES ($1,$2,$3) RETURNING *;`;
-  dictionaryData.map(async function (data) {
-    let populatedData = await query(sqlString, [
-      data.student_id,
-      data.word,
-      data.definition,
-    ]);
-    console.log(populatedData);
-  });
-}
-populateDictionaryData();
+// async function populateDictionaryData() {
+//   const sqlString = `INSERT INTO dictionary (student_id, word,definition) VALUES ($1,$2,$3) RETURNING *;`;
+//   dictionaryData.map(async function (data) {
+//     let populatedData = await query(sqlString, [
+//       data.student_id,
+//       data.word,
+//       data.definition,
+//     ]);
+//     console.log(populatedData);
+//   });
+// }
+// populateDictionaryData();
 
-async function populateAllBooksTable() {
-  const sqlString = `INSERT INTO allBooks (id, student_id, title,author,cover, total_pages) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
-  allBooksData.map(async function (data) {
-    let populatedData = await query(sqlString, [
-      data.id,
-      data.student_id,
-      data.title,
-      data.author,
-      data.cover,
-      data.total_pages,
-    ]);
-    console.log(populatedData);
-  });
-}
-populateAllBooksTable();
+// async function populateAllBooksTable() {
+//   const sqlString = `INSERT INTO allBooks (id, student_id, title,author,cover, total_pages) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
+//   allBooksData.map(async function (data) {
+//     let populatedData = await query(sqlString, [
+//       data.id,
+//       data.student_id,
+//       data.title,
+//       data.author,
+//       data.cover,
+//       data.total_pages,
+//     ]);
+//     console.log(populatedData);
+//   });
+// }
+// populateAllBooksTable();
 
-async function populateSummariesTable() {
-  const sqlString = `INSERT INTO summaries (student_id,book_id,current_page,summary,isComplete,minutes_read) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
-  summaryData.map(async function (data) {
-    let populatedData = await query(sqlString, [
-      data.student_id,
-      data.book_id,
-      data.current_page,
-      data.summary,
-      data.isComplete,
-      data.minutes_read,
-    ]);
-    console.log(populatedData);
-  });
-}
-populateSummariesTable();
+// async function populateSummariesTable() {
+//   const sqlString = `INSERT INTO summaries (student_id,book_id,current_page,summary,isComplete,minutes_read) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
+//   summaryData.map(async function (data) {
+//     let populatedData = await query(sqlString, [
+//       data.student_id,
+//       data.book_id,
+//       data.current_page,
+//       data.summary,
+//       data.isComplete,
+//       data.minutes_read,
+//     ]);
+//     console.log(populatedData);
+//   });
+// }
+// populateSummariesTable();
