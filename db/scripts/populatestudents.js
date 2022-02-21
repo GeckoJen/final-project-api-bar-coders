c8bf67f3ba92db567128c75156c256931f904839
AsmaDage <asmadage@gmail.com>
2022-02-21 10:14:42 +0000

    query import added

--- db/scripts/populatestudents.js
+++ db/scripts/populatestudents.js
@@ -1,4 +1,6 @@
 import { studentData } from "../../data.js";
+import query from "../index.js";
+
 async function populateStudentsTable() {
   const sqlString = `INSERT INTO students (id,name,avatar,class,teacher_id) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
   studentData.map(async function (data) {
