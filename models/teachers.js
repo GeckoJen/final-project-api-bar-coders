639c7093b472f9c1dc1390ca14368284d5e25a63
Jenny Taylor <jennytaylor82@gmail.com>
2022-03-01 18:15:09 +0000

    getclassminutes function complete

--- models/teachers.js
+++ models/teachers.js
@@ -38,18 +38,45 @@ export async function getClassMinutes() {
       const weekNumber = fullArray[0].weekly;
       const thisWeekData = fullArray.filter(
             (entry) => entry.weekly === weekNumber
-      );
+    );
+    
+function getSundayFromWeekNum (weekNum, year) {
+  const sunday = new Date(year, 0, 1 + (weekNum) * 7);
+  while (sunday.getDay() !== 0) {
+    sunday.setDate(sunday.getDate() - 1);
+  }
+  return sunday;
+};
+
+let sunday = getSundayFromWeekNum(weekNumber, new Date().getFullYear());
+
+    console.log(getSundayFromWeekNum(10, 2021));
+
       let minutesArray = [];
-      for (let i = 1; i < 8; i++) {
-            let day = thisWeekData.filter((entry) => entry.daily === i);
+      for (let i = 1; i < 7; i++) {
+          let day = thisWeekData.filter((entry) => entry.daily === i);
+          var ms = new Date(sunday).getTime() + 86400000 * i;
+          var newdate = new Date(ms);
             if (day[0]) {
                   minutesArray.push({
-                        day: i,
+                        date: newdate,
                         minutes: day[0].minutes_total,
                   });
             } else {
-                  minutesArray.push({ day: i, minutes: 0 });
+                  minutesArray.push({ date: newdate, minutes: 0 });
             }
-      }
+    }
+    let day = thisWeekData.filter((entry) => entry.daily === 0);
+    var ms = new Date(sunday).getTime() + 86400000 * 7;
+    var newdate = new Date(ms);
+    if (day[0]) {
+      minutesArray.push({
+        date: newdate,
+        minutes: day[0].minutes_total,
+      });
+    } else {
+      minutesArray.push({ date: newdate, minutes: 0 });
+    }
       return minutesArray;
 }
+
