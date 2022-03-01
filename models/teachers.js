e0e960f1582522f548a103e9e9a2b5f5ff4364fb
codingmohamed <cloudchronicles92@gmail.com>
2022-03-01 15:00:29 +0000

    part of a route done

--- models/teachers.js
+++ models/teachers.js
@@ -1,7 +1,7 @@
 import query from "../db/index.js";
 
 export async function getClassList() {
-  const data = await query(`SELECT
+      const data = await query(`SELECT
     date_part('week', summaries.date_created::date) AS weekly,
     COUNT(DISTINCT summaries.date_created::date),
    
@@ -11,10 +11,32 @@ INNER JOIN students
 ON  summaries.student_id=students.id
 GROUP BY weekly, students.name
 ORDER  BY weekly DESC `);
-  const fullArray = data.rows;
-  const weekNumber = fullArray[0].weekly;
-  const thisWeekData = fullArray.filter((entry) => entry.weekly === weekNumber);
-  const under4Data = thisWeekData.filter((entry) => entry.count < 4);
-  const over4Data = thisWeekData.filter((entry) => entry.count >= 4);
-  return { totalData: thisWeekData, under4Data: under4Data, over4Data: over4Data };
+      const fullArray = data.rows;
+      const weekNumber = fullArray[0].weekly;
+      const thisWeekData = fullArray.filter(
+            (entry) => entry.weekly === weekNumber
+      );
+      const under4Data = thisWeekData.filter((entry) => entry.count < 4);
+      const over4Data = thisWeekData.filter((entry) => entry.count >= 4);
+      return {
+            totalData: thisWeekData,
+            under4Data: under4Data,
+            over4Data: over4Data,
+      };
+}
+
+export async function getClassMinutes() {
+      const data = await query(`SELECT
+  summaries.date_created,
+    date_part('dow', summaries.date_created::date) AS daily,
+      date_part('week', summaries.date_created::date) AS weekly,
+      SUM(Minutes_read) AS minutes_total
+  FROM summaries
+  GROUP BY weekly, daily, summaries.date_created
+  ORDER  BY weekly DESC`);
+      const fullArray = data.rows;
+      const weekNumber = fullArray[0].weekly;
+      const thisWeekData = fullArray.filter(
+            (entry) => entry.weekly === weekNumber
+      );
 }
