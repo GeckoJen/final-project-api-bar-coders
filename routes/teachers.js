35f8b8b7717345f023e522c869c2dcb09228f944
codingmohamed <cloudchronicles92@gmail.com>
2022-03-02 15:10:22 +0000

    individual student data showing

--- routes/teachers.js
+++ routes/teachers.js
@@ -2,21 +2,36 @@ import createError from "http-errors";
 import Router from "express-promise-router";
 const router = Router();
 
-import { getClassList, getClassMinutes, getClassPages, getClassBooksCompleted } from "../models/teachers.js";
+import {
+      getClassList,
+      getClassMinutes,
+      getClassPages,
+      getClassBooksCompleted,
+      getIndividualStudentWeeklyReading,
+} from "../models/teachers.js";
 
 router.get("/class", async function (req, res, next) {
       const data = await getClassList();
-    const minutesReadByClass = await getClassMinutes();
-    const pagesReadByClass = await getClassPages();
-    const booksCompletedByClass = await getClassBooksCompleted();
+      const minutesReadByClass = await getClassMinutes();
+      const pagesReadByClass = await getClassPages();
+      const booksCompletedByClass = await getClassBooksCompleted();
       res.json({
-        success: true,
-        barChartData: data.totalData,
-        classListLessThan4Times: data.under4Data,
-        classList4TimesOrMore: data.over4Data,
-        minutesReadByClass: minutesReadByClass,
-        pagesReadByClass: pagesReadByClass,
-        booksCompletedByClass: booksCompletedByClass,
+            success: true,
+            barChartData: data.totalData,
+            classListLessThan4Times: data.under4Data,
+            classList4TimesOrMore: data.over4Data,
+            minutesReadByClass: minutesReadByClass,
+            pagesReadByClass: pagesReadByClass,
+            booksCompletedByClass: booksCompletedByClass,
+      });
+});
+
+router.get("/student/:id", async function (req, res, next) {
+      const { id } = req.params;
+      const studentWeeklyReading = await getIndividualStudentWeeklyReading(id);
+      res.json({
+            success: true,
+            studentWeeklyReading: studentWeeklyReading,
       });
 });
 
